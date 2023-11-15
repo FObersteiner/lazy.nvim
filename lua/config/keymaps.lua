8b623c52642671a325815e3616200ced4f5730b6
FObersteiner <florian.obersteiner@kit.edu>
2023-11-15 14:23:38 +0100

    add some config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set(
+  "n", -- normal mode
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
+
+vim.keymap.set(
+  "n", --
+  "<leader>bn",
+  ":bnext<CR>",
+  { noremap = true, silent = true, desc = "Next Buffer" }
+)
+
+vim.keymap.set(
+  "n", --
+  "<leader>cL",
+  ":LspRestart<CR>",
+  { noremap = true, silent = true, desc = "Restart LSP" }
+)
