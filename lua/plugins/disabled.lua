d3a448507451eedacdac91c0aaedd8ca79a6f6f7
FObersteiner <f.obersteiner@posteo.de>
2023-11-16 20:06:40 +0100

    disable popups

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,13 @@
+return {
+  { "rcarriga/nvim-notify", enabled = false },
+  --
+  -- this would disable any remaining notificatoins:
+  -- {
+  --   "folke/noice.nvim",
+  --   opts = {
+  --     notify = {
+  --       enabled = false,
+  --     },
+  --   },
+  -- },
+}
