19fb7c45573042332a9f0891be3648c0a15d31b3
FObersteiner <f.obersteiner@posteo.de>
2023-12-26 17:55:32 +0100

    try cursor auto-on/off

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- cursorline on in normal mode
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  callback = function()
+    vim.opt.cursorline = true
+    vim.opt.cursorcolumn = true
+  end,
+})
+-- no Cursorline in insert mode
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+  callback = function()
+    vim.opt.cursorline = false
+    vim.opt.cursorcolumn = false
+  end,
+})
