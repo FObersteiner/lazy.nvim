c063821712c2fd3f7f53561b9b767cbed672d6fd
FObersteiner <f.obersteiner@posteo.de>
2024-06-01 12:31:38 +0200

    update defaults

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    inlay_hints = {
+      enabled = false,
+      exclude = {}, -- filetypes for which you don't want to enable inlay hints
+    },
     servers = {
       -- markdown
       marksman = {
