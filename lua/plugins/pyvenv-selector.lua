b6be07b035c5889cc69309eb9c5c9f298303349e
FObersteiner <florian.obersteiner@kit.edu>
2024-06-03 08:27:43 +0200

    news

--- lua/plugins/pyvenv-selector.lua
+++ lua/plugins/pyvenv-selector.lua
@@ -6,7 +6,7 @@ return {
     -- name = "venv",
     -- auto_refresh = false
   },
-  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  -- event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
     { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "select Python virtual environment" },
