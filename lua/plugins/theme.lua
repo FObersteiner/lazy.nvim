fa2b93ec4b7dfab04becfdde28c88fcc89b842c5
FObersteiner <f.obersteiner@posteo.de>
2024-06-21 14:50:57 +0200

    unused colorschemes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,37 +5,38 @@ return {
   {
     "projekt0n/github-nvim-theme",
   },
-  {
-    "catppuccin/nvim",
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-  },
-  {
-    "kvrohit/rasmus.nvim",
-  },
-  {
-    "tanvirtin/monokai.nvim",
-  },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
-    "hendriknielaender/stardust.nvim",
-  },
+  -- {
+  --   "catppuccin/nvim",
+  -- },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  -- },
+  -- {
+  --   "kvrohit/rasmus.nvim",
+  -- },
+  -- {
+  --   "tanvirtin/monokai.nvim",
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "hendriknielaender/stardust.nvim",
+  -- },
 
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "bamboo",
       -- colorscheme = "catppuccin",
+      -- colorscheme = "github_light",
       -- colorscheme = "gruvbox",
       -- colorscheme = "monokai",
       -- colorscheme = "rasmus",
