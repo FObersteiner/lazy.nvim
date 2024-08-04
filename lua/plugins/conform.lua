d54366aeffeeae155b77e41ecf4bcb21c2306fb5
FObersteiner <f.obersteiner@posteo.de>
2024-08-04 12:58:06 +0200

    dont set format_on_save for conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -22,10 +22,10 @@ return {
       ["vue"] = { "prettier" },
       ["yaml"] = { "prettier" },
     },
-    format_on_save = {
-      lsp_fallback = true,
-      async = false,
-      timeout_ms = 500,
-    },
+    -- format_on_save = {
+    --   lsp_fallback = true,
+    --   async = false,
+    --   timeout_ms = 500,
+    -- },
   },
 }
