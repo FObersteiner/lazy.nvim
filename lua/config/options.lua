f0991242b9838db2bfceab1e89bdc375ca154b8f
FObersteiner <f.obersteiner@posteo.de>
2024-11-10 14:34:04 +0100

    py

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@
 --
 vim.opt.wrap = true -- line wrap
 vim.opt.winbar = "%=%m %f"
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
