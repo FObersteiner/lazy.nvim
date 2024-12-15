9c5bf40107fb8e821ca984b20da2297f2d27ab0d
FObersteiner <florian.obersteiner@kit.edu>
2024-12-15 13:00:53 +0100

    nvim-cmp to blink

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,67 @@
+return {
+  "saghen/blink.cmp",
+  lazy = false, -- lazy loading handled internally
+  -- optional: provides snippets for the snippet source
+  dependencies = "rafamadriz/friendly-snippets",
+
+  -- use a release tag to download pre-built binaries
+  version = "v0.*",
+  -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
+    keymap = {
+      preset = "enter",
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      --
+      --   -- disable a keymap from the preset
+      --   ['<C-e>'] = {},
+      --
+      --   -- show with a list of providers
+      --   ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
+      --
+      --   -- note that your function will often be run in a "fast event" where most vim.api functions will throw an error
+      --   -- you may want to wrap your function in `vim.schedule` or use `vim.schedule_wrap`
+      --   ['<C-space>'] = { function(cmp) vim.schedule(function() your_behavior end) },
+      --
+      --   -- optionally, define different keymaps for cmdline
+      --   cmdline = {
+      --     preset = 'super-tab'
+      --   }
+    },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
+    },
+
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      -- optionally disable cmdline completions
+      -- cmdline = {},
+    },
+
+    -- experimental signature help support
+    -- signature = { enabled = true }
+  },
+  -- allows extending the providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { "sources.default" },
+}
